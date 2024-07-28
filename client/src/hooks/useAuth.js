904a42ec01bcab23c47f1dbda90e5e4852c4bb13
ivan9paunov <vankatapaunov@gmail.com>
2024-07-29 01:07:47 +0300

    Add custom hooks

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+
+import { login } from "../api/auth-api.js";
+import { AuthContext } from "../contexts/AuthContext.js";
+
+export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
+    const loginHandler = async (email, password) => {
+        const result = await login(email, password);
+
+        changeAuthState(result);
+    }
+
+    return loginHandler;
+};
---
+++ client/src/hooks/useReports.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import reportsAPI from "../api/reports-api.js";
+
+export function useGetAllReports() {
+    const [reports, setReports] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await reportsAPI.getAll();
+
+                setReports(result);
+            } catch (error) {
+                alert(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+    }, []);
+
+    return {
+        reports,
+        isLoading
+    }
+}
+
+export function useGetOneReport(reportId) {
+    const [report, setReport] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const result = await reportsAPI.getOne(reportId);
+
+            setReport(result);
+        })();
+    }, [reportId]);
+
+    return [
+        report,
+        setReport
+    ];
+}
